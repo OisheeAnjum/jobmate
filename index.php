6a7bd6cfcffce489cc8bccd9d2f8da5f8fc03ffa
oishee42@gmail.com <oishee42@gmail.com>
2021-11-08 18:21:40 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php  include("nav.php");?>
+<div class="content">
+    <div class="container px-4 px-lg-5">
+        <!-- Heading Row-->
+        <div class="row gx-4 gx-lg-5 align-items-center my-5">
+            <div class="col-lg-6">
+                <h2 class="font-weight-light text-capitalize">A digital platform <br> where talent meets <span class="hl-text">opportunity</span></h2>
+                <p><b>We have someting big for you who want to level up. <br>Find the
+                        right fit and start your career.</b>
+                </p>
+                <a href="details.php" class="btn btn-success" id="navreg">Register</a>
+
+            
+        </div>
+            <div class="col-lg-6">
+                <img class="img-fluid rounded mb-4 mb-lg-0" src="assets/work.gif" />
+            </div>
+           
+</div>
+
+</div>
+<?php  include("footer.php");?>
+
